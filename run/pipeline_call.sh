ce1bd2d192ac38919cd68daa4ac0690b76e6acac
Eric Levy <edlevy@ucsd.edu>
2014-01-31 09:23:29 -0800

    MAGI run parameters and shell script

---
+++ run/pipeline_call.sh
@@ -0,0 +1,2 @@
+#testrun for pipeline
+perl -I /storage/home/elevy/magi_pipeline/bin  /storage/home/elevy/magi_pipeline/bin/pipeline.pl  -dir /storage/home/elevy/magi_pipeline/run/example_input  -config  /storage/home/elevy/magi_pipeline/run/parameters.txt -out /storage/home/elevy/magi_pipeline/run/example_run
