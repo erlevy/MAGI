70cab05bee6231809fbd9829459c8ef8c203f67a
Eric Levy <edlevy@ucsd.edu>
2016-10-12 16:06:21 -0700

    Fixing Docker input files

--- run/pipeline_call.sh
+++ run/pipeline_call.sh
@@ -1,2 +1,2 @@
 #testrun for pipeline
-perl -I /MAGI/bin  /MAGI/bin/pipeline.pl  -dir /MAGI/input/  -config  /MAGI/run/parameters.txt -out /MAGI/example_run
+perl -I /MAGI/bin  /MAGI/bin/pipeline.pl  -dir /MAGI/input/  -config  /MAGI/run/parameters.txt -out /MAGI/example_run/
