3e64f8dc7f0096b535772e933a83cf77540e9db6
Eric Levy <edlevy@ucsd.edu>
2016-10-12 15:24:23 -0700

    Updating for Docker version

--- run/pipeline_call.sh
+++ run/pipeline_call.sh
@@ -1,2 +1,2 @@
 #testrun for pipeline
-perl -I /storage/home/elevy/magi_pipeline/bin  /storage/home/elevy/magi_pipeline/bin/pipeline.pl  -dir /storage/home/elevy/magi_pipeline/run/example_input  -config  /storage/home/elevy/magi_pipeline/run/parameters.txt -out /storage/home/elevy/magi_pipeline/run/example_run
+perl -I /MAGI/bin  /MAGI/bin/pipeline.pl  -dir /MAGI/example_input  -config  /MAGI/run/parameters.txt -out /MAGI/example_run
